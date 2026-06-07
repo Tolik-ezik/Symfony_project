09219ed189b3cdef8fab90c61439ad159de1b9d6
Tolik-ezik <kristinahankey@gmail.com>
2026-06-07 14:41:37 +0300

    Getting started with symphony, working with the database

---
+++ src/Command/GoCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'GoCommand',
+    description: 'Add a short description for your command',
+)]
+class GoCommand extends Command
+{
+    public function __construct(private PostReposytory $postReposytory)
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        
+        return Command::SUCCESS;
+    }
+}
